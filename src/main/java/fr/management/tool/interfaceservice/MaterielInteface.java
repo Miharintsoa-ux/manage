44c35d7f43326616cb4503e467c4a020f9eb73e2
Miharintsoa-ux <miharintsoap19.aps1b@gmail.com>
2023-06-26 12:03:27 +0300

    add-read-abs-meth

--- src/main/java/fr/management/tool/interfaceservice/MaterielInteface.java
+++ src/main/java/fr/management/tool/interfaceservice/MaterielInteface.java
@@ -1,8 +1,12 @@
 package fr.management.tool.interfaceservice;
 
+
+import java.util.List;
+
 import fr.management.tool.entity.Materiel;
 
 public interface MaterielInteface {
     public Materiel createMateriel(Materiel materiel);
+    public List<Materiel> readMateriels();
     
 }
