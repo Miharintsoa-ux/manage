43341ed9c41a94a71a6733aac23b9d8bb64e6086
Miharintsoa-ux <miharintsoap19.aps1b@gmail.com>
2023-06-26 14:28:15 +0300

    intervenant-interface

---
+++ src/main/java/fr/management/tool/interfaceservice/IntervenantInterface.java
@@ -0,0 +1,7 @@
+package fr.management.tool.interfaceservice;
+
+import fr.management.tool.entity.Intervenant;
+
+public interface IntervenantInterface {
+    public Intervenant createIntervenant(Intervenant intervenant);    
+}
