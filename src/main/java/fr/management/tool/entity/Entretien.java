44cb7bd6036b3026cc05364e7e890c03a76ddbbe
Miharintsoa-ux <miharintsoap19.aps1b@gmail.com>
2023-06-26 16:52:34 +0300

    review-entity

--- src/main/java/fr/management/tool/entity/Entretien.java
+++ src/main/java/fr/management/tool/entity/Entretien.java
@@ -7,6 +7,9 @@ import com.fasterxml.jackson.annotation.JsonFormat.Shape;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -17,6 +20,8 @@ import lombok.Data;
 @Table(name = "entretien")
 public class Entretien {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @JsonFormat(pattern = "yyyy-MM-dd", shape = Shape.STRING)
