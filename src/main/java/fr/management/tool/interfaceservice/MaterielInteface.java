f5eceff5687b5ad5c1d143c73ecc307ca896598c
Miharintsoa-ux <miharintsoap19.aps1b@gmail.com>
2023-06-26 11:52:56 +0300

    materiel-service-interface

---
+++ src/main/java/fr/management/tool/interfaceservice/MaterielInteface.java
@@ -0,0 +1,8 @@
+package fr.management.tool.interfaceservice;
+
+import fr.management.tool.entity.Materiel;
+
+public interface MaterielInteface {
+    public Materiel createMateriel(Materiel materiel);
+    
+}
